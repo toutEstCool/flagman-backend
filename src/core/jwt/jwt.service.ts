de4c643f5c59b1dcf69ac67f3160ce99e2d2b9d0
Danil Panarin <danilpanarin@MacBook-Air-Danil.local>
2025-04-06 21:08:44 +0600

    feat: save jwt in db

--- src/core/jwt/jwt.service.ts
+++ src/core/jwt/jwt.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtService as Jwt } from '@nestjs/jwt'
+import { addDays } from 'date-fns'
+
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class JwtService {
   public constructor(
     private readonly jwtService: Jwt,
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService
   ) {}
 
   public async issueTokens(user: { id: string; phone: string }) {
@@ -18,6 +22,14 @@ export class JwtService {
 
     const refreshToken = await this.jwtService.signAsync(payload, {
       expiresIn: this.configService.getOrThrow<string>('JWT_REFRESH_EXPIRES_IN')
+    })
+
+    await this.prismaService.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: user.id,
+        expiresAt: addDays(new Date(), 7)
+      }
     })
 
     return { accessToken, refreshToken }
